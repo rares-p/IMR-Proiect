a73aeab0e5c24a519a02637d7186c1256bd60b56
stefan1044 <stefan.harabagiu2000@gmail.com>
2023-10-22 21:47:56 +0300

    finished project

--- Assets/TargetSpawner.cs
+++ Assets/TargetSpawner.cs
@@ -1,34 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject targetPrefab;
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private Text scoreText;
     private GameObject target;
+    
     public int Score { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = SpawnTargetAtRandomPosition();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.U))
-            target = SpawnTargetAtRandomPosition();
+        scoreText.GetComponent<Text>().text = Score.ToString();
+        Debug.Log(Score);
     }
 
     public GameObject SpawnTargetAtRandomPosition()
     {
-        var randomPosition = new Vector3(0.5f, Random.Range(-0.4f, 0.4f), Random.Range(-0.47f, 0.47f));
+        var randomPosition = new Vector3(0.5f, Random.Range(-0.37f, 0.37f), Random.Range(-0.47f, 0.47f));
         GameObject newTarget = Instantiate(targetPrefab);
         newTarget.transform.parent = this.transform;
         newTarget.transform.localPosition = randomPosition;
         newTarget.transform.localRotation = Quaternion.Euler(0, 90, 0);
-        newTarget.transform.localScale = new Vector3(0.015f, 0.05f, 0.0001f);
+        newTarget.transform.localScale = new Vector3(0.17f, 0.15f, 0.0001f);
         Debug.Log(newTarget.transform.position);
 
         return newTarget;
@@ -36,6 +44,10 @@ public class TargetSpawner : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Score += (int) Mathf.Abs(Vector2.Distance(new Vector2(target.transform.position.y, target.transform.position.z), new Vector2(collision.gameObject.transform.position.y, collision.transform.position.z)));
+        Score += (int) (1 / (Vector2.Distance(new Vector2(target.transform.position.y, target.transform.position.z), new Vector2(collision.gameObject.transform.position.y, 
+            collision.transform.position.z)) + 0.1) * 10
+            + Vector3.Distance(this.transform.position, player.transform.position) * 250);
+        Destroy(target);
+        target = SpawnTargetAtRandomPosition();
     }
 }
---
+++ Assets/Throw.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Throw : MonoBehaviour
+{
+    [SerializeField] float _throwForce = 1000f;
+
+    List<Vector3> _trackedPositions = new List<Vector3>();
+    bool _isHeld;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
